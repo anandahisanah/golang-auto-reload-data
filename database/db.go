592618f6905c5bbb8e862d2f095a533c2edd77d3
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-01 16:41:47 +0800

    build: database

---
+++ database/db.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"assignment-3/models"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "postgres"
+	dbPort   = "5432"
+	dbName   = "assignment_2"
+	db       *gorm.DB
+	err      error
+)
+
+func StartDB() {
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, dbPort)
+	dsn := config
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("error connecting to database:", err)
+	}
+
+	fmt.Println("success connecting to database")
+	db.Debug().AutoMigrate(models.Log{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
